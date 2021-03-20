879bb9f1a93831dcf4eb276e20e1e1f9540b62f9
Ralf Lang <lang@b1-systems.de>
2021-03-20 14:53:00 +0000

    Still fails on github...

--- test/Horde/Support/bootstrap.php
+++ test/Horde/Support/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
-if (!class_exists(\Horde_Test_Bootstrap::class)) {
-    require_once 'Horde/Test/Bootstrap.php';
-}
 $composerAutoload = __DIR__ . '/../../../vendor/autoload.php';
-
 // Catch the case where we use composer but phpunit is installed externally
 if (file_exists($composerAutoload)) {
     require_once $composerAutoload;
 }
+
+if (!class_exists(\Horde_Test_Bootstrap::class)) {
+    require_once 'Horde/Test/Bootstrap.php';
+}
+
 require_once 'Helper/ConsistentHashInstrumented.php';
 Horde_Test_Bootstrap::bootstrap(__DIR__);
